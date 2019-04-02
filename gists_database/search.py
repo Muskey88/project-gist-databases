516674509c73267385d3f00a8a527bd27d9fedba
ChrisM <cmccluskey@gmail.com>
2019-04-02 10:28:09 +0000

    Database project passing all tests

--- gists_database/search.py
+++ gists_database/search.py
@@ -1,4 +1,41 @@
 from .models import Gist
 
+def op_to_use(op):
+    return {
+        'gt': '>',
+        'gte': '>=',
+        'lt': '<',
+        'lte': '<=',
+    }[op]
+
 def search_gists(db_connection, **kwargs):
-    pass
+    basic = 'SELECT * FROM gists'
+    results = []
+    filters = []
+    params = {}
+    if kwargs:
+        for name, value in kwargs.items():
+            if 'created_at' in kwargs and '__' not in kwargs:
+                filters.append('datetime(%s) == datetime(:%s)' % (name, name))
+                params[name] = value
+
+            elif '__' in name:
+                att, operator = name.split('__')
+                params[att] = value
+                op = op_to_use(operator)
+                filters.append('datetime(%s) %s datetime(:%s)' % (att, op, att))
+
+            else:
+                filters.append('%s = :%s' % (name, name))
+                params[name] = value
+
+        basic +=' WHERE '
+        basic += ' AND '.join(filters)
+        cursor = db_connection.execute(basic, params)
+    else:
+        cursor = db_connection.execute(basic)
+    
+    for gist in cursor:
+        results.append(Gist(gist))
+        
+    return results
